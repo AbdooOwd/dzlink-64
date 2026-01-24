6b4f229b20bfb1761183d119fb36afee42886bcd
AbdooOwd <benari102@gmail.com>
2026-01-24 12:17:17 +0100

    [state] play: tesing cam & widescreen

--- src/code/states/play_state.c
+++ src/code/states/play_state.c
@@ -5,8 +5,10 @@
 #include <lib/types.h>
 #include <lib/utils.h>
 #include "game.h"
+#include "joypad.h"
 #include "rdpq_text.h"
 #include "t3d/t3dmath.h"
+#include <video/dz_display.h>
 #include <t3d/t3d.h>
 #include <globals.h>
 #include <states/play_state.h>
@@ -38,6 +40,11 @@ void PlayState_Main(void* state) {
   PlayState* this = (PlayState*) state;
   Input* input = &gMasterState.input;
 
+
+  if (joypad_get_buttons_pressed(JOYPAD_DEFAULT_PORT).l) {
+    display_switch_widescreen();
+  }
+
   t3d_frame_start();
 
   t3d_screen_clear_color(RGBA32(80, 80, 80, 0xFF));
@@ -45,11 +52,13 @@ void PlayState_Main(void* state) {
 
   Actor_UpdateAll(this->actorCtx, this);
 
-  Camera_Rotate(&this->camera, input->stick_x * 2.0f, input->stick_y * 2.0f);
-  Camera_TranslateFree(&this->camera, VEC3(input->cstick_x * 0.01f, 0, input->cstick_y * 0.01f));
+  Camera_Rotate(&this->camera, input->stick_x * 3.0f, input->stick_y * 3.0f);
+  Camera_TranslateFree(&this->camera, &VEC3(input->cstick_x * 0.01f, 0, input->cstick_y * 0.01f));
   Camera_Attach(&this->camera);
 
   rdpq_text_printf(NULL, FONTID_DBG_VAR, 10.0f, 10.0f, "FPS: %.02f", display_get_fps());
+  rdpq_text_printf(NULL, FONTID_DBG_VAR, 10.0f, 20.0f, "Aspect Ratio: %.02f", Camera_GetAspectRatio(&this->camera));
+  rdpq_text_printf(NULL, FONTID_DBG_VAR, 10.0f, display_get_height() - 10.0f, "Press L for widescreen");
 }
 
 void PlayState_Destroy(void* state) {
